907f56678750d24ab504610f554cf34fa47bde2c
Jakob Löhnertz <mail@jakob.codes>
2019-11-25 16:29:17 +0100

    Create a scaffold for the main resource

--- src/main/kotlin/resource/AnalysisResource.kt
+++ src/main/kotlin/resource/AnalysisResource.kt
@@ -8,13 +8,20 @@ import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
 import io.ktor.routing.route
+import model.AnalysisRequest
+import model.Unit
+import java.io.File
 
 
 fun Route.analysis(controller: AnalysisController) {
     route("/analysis") {
         get("/") {
-            controller.analyze()
-            call.respond("WORKS!")
+            val analysisRequest = AnalysisRequest(
+                projectRoot = File(call.parameters["projectRoot"]!!.toString()),
+                massThreshold = call.parameters["massThreshold"]!!.toInt()
+            )
+            val clones: List<Set<Unit>> = controller.analyze(analysisRequest)
+            call.respond(clones.map { clone -> clone.map { Pair(it.node.toString(), it.location.toString()) } })
         }
     }
 
