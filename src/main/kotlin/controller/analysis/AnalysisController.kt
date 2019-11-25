39101183cca6eca5be0e59be60785b846457e0cb
Jakob Löhnertz <mail@jakob.codes>
2019-11-25 16:30:40 +0100

    Scaffold the major method of the 'AnalysisController'

--- src/main/kotlin/controller/analysis/AnalysisController.kt
+++ src/main/kotlin/controller/analysis/AnalysisController.kt
@@ -1,7 +1,16 @@
 package controller.analysis
 
+import controller.analysis.detection.CloneDetector
+import controller.analysis.parsing.Parser
+import model.AnalysisRequest
+import model.Unit
+
 
 class AnalysisController {
-    fun analyze() {
+    fun analyze(analysisRequest: AnalysisRequest): List<Set<Unit>> {
+        val startTime: Long = System.currentTimeMillis()
+        val units: List<Unit> = Parser(analysisRequest.projectRoot).parse()
+        val clones: List<Set<Unit>> = CloneDetector(units, analysisRequest.massThreshold).detectClones()
+        return clones.also { println("The analysis of project '${analysisRequest.projectRoot}' took ${(System.currentTimeMillis() - startTime) / 1000} seconds.") }
     }
 }
