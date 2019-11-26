fccdc4bf2f986b0bede18cea0185c5b321ce3df3
Jakob Löhnertz <mail@jakob.codes>
2019-11-26 17:24:59 +0100

    Implement the 'exampleClones' metric

--- src/main/kotlin/controller/analysis/detection/CloneMetricsCalculator.kt
+++ src/main/kotlin/controller/analysis/detection/CloneMetricsCalculator.kt
@@ -2,10 +2,21 @@ package controller.analysis.detection
 
 import model.CloneMetrics
 import utility.Clone
+import utility.JsonClone
 
 
 class CloneMetricsCalculator(private val clones: List<Clone>) {
     fun calculateMetrics(): CloneMetrics {
-        return CloneMetrics()
+        return CloneMetrics(
+            exampleClones = selectRandomExampleClones()
+        )
+    }
+
+    private fun selectRandomExampleClones(): List<JsonClone> {
+        return clones.shuffled().take(EXAMPLE_CLONE_AMOUNT).map { Pair(it.first.convertToJsonUnit(), it.second.convertToJsonUnit()) }
+    }
+
+    companion object Constants {
+        private const val EXAMPLE_CLONE_AMOUNT = 10
     }
 }
