8dfc89a67935092a0dcbf1ec6c5f2d10c76e31bb
NuriaBruchTarrega <nuria.bruch@gmail.com>
2019-12-02 13:57:23 +0100

    Update class Visitor to object and add second visit method

--- src/main/kotlin/controller/analysis/parsing/Visitor.kt
+++ src/main/kotlin/controller/analysis/parsing/Visitor.kt
@@ -5,17 +5,22 @@ import com.github.javaparser.ast.visitor.TreeVisitor
 import model.Unit
 
 
-class Visitor {
-    private val nodes = ArrayList<Node>()
-
-    private val visitor: TreeVisitor = object : TreeVisitor() {
-        override fun process(node: Node) {
-            nodes.add(node)
-        }
+object Visitor {
+    fun visit(node: Node, nodeConversionFunction: (Node) -> Unit): List<Unit> {
+        val nodes: ArrayList<Node> = arrayListOf()
+        RecursiveVisitor(nodes).also { it.visitPreOrder(node) }
+        return nodes.map { nodeConversionFunction(it) }.also { nodes.clear() }
     }
 
-    fun visit(node: Node, nodeConversionFunction: (Node) -> Unit): List<Unit> {
-        visitor.visitPreOrder(node)
-        return nodes.map { nodeConversionFunction(it) }.also { nodes.clear() }
+    fun visit(node: Node): List<Node> {
+        val nodes: ArrayList<Node> = arrayListOf()
+        RecursiveVisitor(nodes).also { it.visitPreOrder(node) }
+        return nodes
+    }
+}
+
+class RecursiveVisitor(private val nodes: ArrayList<Node>) : TreeVisitor() {
+    override fun process(node: Node) {
+        nodes.add(node)
     }
 }
