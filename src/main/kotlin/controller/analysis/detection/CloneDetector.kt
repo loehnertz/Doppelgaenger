c3aafc5f88c06ecf2f1812fd72b6b38a1cb2e5c8
NuriaBruchTarrega <nuria.bruch@gmail.com>
2019-12-09 15:00:27 +0100

    Rename sequenceGroups as sequenceClass and sequenceClass converted to set

--- src/main/kotlin/controller/analysis/detection/CloneDetector.kt
+++ src/main/kotlin/controller/analysis/detection/CloneDetector.kt
@@ -21,10 +21,14 @@ class CloneDetector(private val units: List<Unit>, massThreshold: Int?, private 
         return clones to cloneClasses
     }
 
+    fun filterOutClonesIncludedInSequenceClasses(cloneClasses: List<Set<Unit>>, sequenceCloneClasses: List<Set<List<Unit>>>): List<Set<Unit>> {
+        return cloneClasses.filter { cloneClass -> sequenceCloneClasses.any{ sequenceCloneClass -> cloneClassIncludedInSequenceCloneClass(cloneClass, sequenceCloneClass) } }
+    }
+
     // This is still a draft
-    fun findSiblingClones(clones: List<Clone>): ArrayList<List<List<Node>>> {
+    fun findSequenceCloneClasses(clones: List<Clone>): ArrayList<Set<List<Node>>> {
         val sequences: List<List<Node>> = clones.flatMap { it.toList() }.toSet().map { it.node.getAllLineSiblings() }.distinct().filter { it.size > 1 }
-        val cloneSequencesGroups: ArrayList<List<List<Node>>> = arrayListOf()
+        val cloneSequencesClasses: ArrayList<Set<List<Node>>> = arrayListOf()
 
         val minimumSequenceLengthThreshold = 2
         val maximumSequenceLength: Int = sequences.maxBy { it.size }!!.size
@@ -32,30 +36,27 @@ class CloneDetector(private val units: List<Unit>, massThreshold: Int?, private 
         for (k: Int in (minimumSequenceLengthThreshold..maximumSequenceLength).reversed()) {
             val subsequencesOfLengthK = sequences.flatMap { it.windowed(k) }.filter { subsequence -> subsequence.any { it.calculateMass() > massThreshold } }
             val buckets = subsequencesOfLengthK.groupBy { subsequence -> subsequence.map { it.leniantHashCode(CloneType.TWO) }.hashCode() }.map { it.value }.filter { it.size > 1 }
-            buckets.forEach { filterBucket(it, cloneSequencesGroups) }
+            buckets.forEach { filterBucket(it, cloneSequencesClasses) }
         }
 
-        return cloneSequencesGroups
+        return cloneSequencesClasses
     }
 
-    private fun filterBucket(bucket: List<List<Node>>, cloneSequencesGroups: ArrayList<List<List<Node>>>) {
-        val cloneGroup: List<List<Node>> = bucket.cartesianProduct() // Get clone pairs
+    private fun filterBucket(bucket: List<List<Node>>, cloneSequencesClasses: ArrayList<Set<List<Node>>>) {
+        val cloneGroup: Set<List<Node>> = bucket.cartesianProduct() // Get clone pairs
                 .filter { listOf(
-                        sequenceIncludedInList(it.first, cloneSequencesGroups),
-                        sequenceIncludedInList(it.second, cloneSequencesGroups)
+                        sequenceIncludedInList(it.first, cloneSequencesClasses),
+                        sequenceIncludedInList(it.second, cloneSequencesClasses)
                 ).any{ isIncluded -> !isIncluded } } // filter out subsequences of already found subsequences clones
                 .filter { calculateSequenceSimilarity(it.first, it.second, massThreshold) > similarityThreshold } // Similarity check
                 .flatMap { listOf(it.first, it.second) } // flatten to obtain the clone group
+                .toSet()
 
-        if (cloneGroup.isNotEmpty()) cloneSequencesGroups.add(cloneGroup)
+        if (cloneGroup.isNotEmpty()) cloneSequencesClasses.add(cloneGroup)
     }
 
-    private fun sequenceIncludedInList(sequence: List<Node>, cloneSequencesGroups: ArrayList<List<List<Node>>>): Boolean {
-        return cloneSequencesGroups.flatten().filter { it.size == sequence.size + 1 }.any { it.containsAll(sequence) }
-    }
-
-    fun filterOutClonesIncludedInSequenceClasses(cloneClasses: List<Set<Unit>>, sequenceCloneClasses: List<Set<List<Unit>>>): List<Set<Unit>> {
-        return cloneClasses.filter { cloneClass -> sequenceCloneClasses.any{ sequenceCloneClass -> cloneClassIncludedInSequenceCloneClass(cloneClass, sequenceCloneClass) } }
+    private fun sequenceIncludedInList(sequence: List<Node>, cloneSequencesClasses: ArrayList<Set<List<Node>>>): Boolean {
+        return cloneSequencesClasses.flatten().filter { it.size == sequence.size + 1 }.any { it.containsAll(sequence) }
     }
 
     private fun cloneClassIncludedInSequenceCloneClass(cloneClass: Set<Unit>, sequenceCloneClass: Set<List<Unit>>): Boolean {
