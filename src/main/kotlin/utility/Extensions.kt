351f6386e3107340b7b73c9146a6f8b80bb407b3
Jakob Löhnertz <mail@jakob.codes>
2019-12-09 10:28:12 +0100

    Add an extension function to the 'Node' class to retrieve all of its sibling nodes

--- src/main/kotlin/utility/Extensions.kt
+++ src/main/kotlin/utility/Extensions.kt
@@ -1,8 +1,12 @@
 package utility
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.javaparser.Position
 import com.github.javaparser.ast.CompilationUnit
 import com.github.javaparser.ast.Node
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.coroutineScope
 import model.CloneType
 import java.nio.file.Path
 import java.util.*
@@ -11,6 +15,10 @@ import java.util.*
 fun <T : Any> Optional<T>.toNullable(): T? = this.orElse(null)
 
 fun Any.toJson(): String = ObjectMapper().writeValueAsString(this)
+
+suspend fun <A, B> Iterable<A>.mapConcurrently(transform: suspend (A) -> B): List<B> = coroutineScope {
+    map { async { transform(it) } }.awaitAll()
+}
 
 inline fun <T, R> Pair<T, T>.map(transform: (T) -> R): Pair<R, R> {
     val pairList: List<R> = this.toList().map { transform(it) }
@@ -42,6 +50,22 @@ fun Node.getAllParentNodes(): Set<Node> {
         setOf()
     } else {
         this.parentNode.get().getAllParentNodes().plus(this.parentNode.get())
+    }
+}
+
+fun Node.getAllLineSiblings(): List<Node> {
+    return if (this.parentNode.isEmpty) {
+        listOf()
+    } else {
+        this.parentNode.get().childNodes.sortedWith(Comparator { s1, s2 ->
+            val beginS1: Position = s1.range.get().begin
+            val beginS2: Position = s2.range.get().begin
+            if (beginS1.line == beginS2.line) {
+                return@Comparator beginS1.column.compareTo(beginS2.column)
+            } else {
+                return@Comparator beginS1.line.compareTo(beginS2.line)
+            }
+        })
     }
 }
 
