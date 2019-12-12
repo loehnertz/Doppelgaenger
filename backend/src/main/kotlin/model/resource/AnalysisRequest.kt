50ea0647e1b699bbf605ab77fc03a626f724ce35
Jakob Löhnertz <mail@jakob.codes>
2019-12-12 23:33:12 +0100

    Implement the 'verify' method of the 'AnalysisRequest'

--- backend/src/main/kotlin/model/resource/AnalysisRequest.kt
+++ backend/src/main/kotlin/model/resource/AnalysisRequest.kt
@@ -12,6 +12,8 @@ data class AnalysisRequest(
     val massThreshold: Int?
 ) {
     fun verify() {
-        TODO("Implement")
+        check(projectRoot.exists()) { "Project root does not exist" }
+        check(similarityThreshold in 0.0..1.0) { "Similarity threshold has to be a percentage between 0 and 100" }
+        if (massThreshold != null) check(massThreshold >= 0) { "Mass threshold has to be a positive integer" }
     }
 }
