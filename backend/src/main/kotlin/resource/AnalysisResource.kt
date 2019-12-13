921a58d88e83d091249953e44ae3a8731186cca7
Jakob Löhnertz <mail@jakob.codes>
2019-12-13 12:03:12 +0100

    Add basic exception handling to the 'AnalysisResource' to respond with the appropriate HTTP status code
    Signed-off-by: Jakob Löhnertz <mail@jakob.codes>

--- backend/src/main/kotlin/resource/AnalysisResource.kt
+++ backend/src/main/kotlin/resource/AnalysisResource.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import controller.analysis.AnalysisController
 import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
 import io.ktor.response.respond
 import io.ktor.routing.Route
 import io.ktor.routing.get
@@ -19,22 +20,28 @@ import java.io.File
 fun Route.analysis(controller: AnalysisController) {
     route("/analysis") {
         get("/") {
-            val projectDirectory: File = when (ProjectType.getProjectTypeByName(call.parameters["projectType"]!!.toString())) {
-                ProjectType.LOCAL -> File(call.parameters["projectRoot"]!!.toString().removeSuffix("/"), AnalysisController.normalizeBasePath(call.parameters["basePath"]!!.toString()))
-                ProjectType.GIT   -> File(AnalysisController.cloneRepository("git@github.com:kevinvandervlist/SE.git"), AnalysisController.normalizeBasePath(call.parameters["basePath"]!!.toString()))
+            try {
+                val projectDirectory: File = when (ProjectType.getProjectTypeByName(call.parameters["projectType"]!!.toString())) {
+                    ProjectType.LOCAL -> File(call.parameters["projectRoot"]!!.toString().removeSuffix("/"), AnalysisController.normalizeBasePath(call.parameters["basePath"]!!.toString()))
+                    ProjectType.GIT   -> File(AnalysisController.cloneRepository(call.parameters["projectRoot"]!!.toString()), AnalysisController.normalizeBasePath(call.parameters["basePath"]!!.toString()))
+                }
+
+                val request: AnalysisRequest = AnalysisRequest(
+                    basePath = AnalysisController.normalizeBasePath(call.parameters["basePath"]!!.toString()),
+                    projectRoot = projectDirectory,
+                    cloneType = getCloneTypeByName(call.parameters["cloneType"] ?: CloneType.ONE.toString()),
+                    similarityThreshold = call.parameters["similarityThreshold"]!!.toDouble() / 100,
+                    massThreshold = call.parameters["massThreshold"]?.toInt()
+                ).also { it.verify() }
+
+                val response: AnalysisResponse = controller.analyze(request)
+
+                call.respond(response).also { AnalysisController.writeResponseToFile(request, response) }
+            } catch (e: IllegalStateException) {
+                call.respond(HttpStatusCode.BadRequest, e.localizedMessage)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.InternalServerError, e.localizedMessage)
             }
-
-            val request = AnalysisRequest(
-                basePath = AnalysisController.normalizeBasePath(call.parameters["basePath"]!!.toString()),
-                projectRoot = projectDirectory,
-                cloneType = getCloneTypeByName(call.parameters["cloneType"] ?: CloneType.ONE.toString()),
-                similarityThreshold = call.parameters["similarityThreshold"]!!.toDouble() / 100,
-                massThreshold = call.parameters["massThreshold"]?.toInt()
-            ).also { it.verify() }
-
-            val response: AnalysisResponse = controller.analyze(request)
-
-            call.respond(response).also { AnalysisController.writeResponseToFile(request, response) }
         }
     }
 
