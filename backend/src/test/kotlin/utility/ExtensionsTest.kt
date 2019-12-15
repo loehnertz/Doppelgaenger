3ab7e9e72a63d885f99505f47e78595c2c2ecd29
Jakob Löhnertz <mail@jakob.codes>
2019-12-15 18:50:11 +0100

    Add more test for the extension functions

--- backend/src/test/kotlin/utility/ExtensionsTest.kt
+++ backend/src/test/kotlin/utility/ExtensionsTest.kt
@@ -13,7 +13,60 @@ import java.util.*
 internal class ExtensionsTest {
 
     @Nested
+    inner class CollectionTest {
+
+        @Test
+        fun cartesianProduct() {
+            val value: List<Pair<Int, Int>> = listOf(1, 2, 3).cartesianProduct()
+            assertThat(value).contains(Pair(1, 2), Pair(1, 3), Pair(2, 3))
+            assertThat(value.flatMap { it.toList() }).contains(1, 2, 3)
+        }
+    }
+
+    @Nested
+    inner class StringTest {
+
+        private val regularLine = "int a = 1;"
+        private val sourceCode = "/**\n" +
+                                 "     * Returns a string\n" +
+                                 "     *\n" +
+                                 "     * @return A string\n" +
+                                 "     */\n" +
+                                 "    public static String hasDocstring() {\n" +
+                                 "                                           " +
+                                 "        return \"TEST\";\n" +
+                                 "    }"
+
+        @Test
+        fun isSingleLineJavaCommentLine() {
+            val commentLine = "// THIS IS A JAVA COMMENT"
+
+            assertThat(regularLine.isSingleLineJavaCommentLine()).isFalse()
+            assertThat(commentLine.isSingleLineJavaCommentLine()).isTrue()
+        }
+
+        @Test
+        fun isBlankLine() {
+            val blankLine = "\t\t"
+
+            assertThat(regularLine.isBlankLine()).isFalse()
+            assertThat(blankLine.isBlankLine()).isTrue()
+        }
+
+        @Test
+        fun filterOutBlankLinesAndJavaComments() {
+            assertThat(sourceCode.filterOutBlankLinesAndJavaComments().replace(" ", "")).isEqualTo("publicstaticStringhasDocstring(){\nreturn\"TEST\";\n}")
+        }
+
+        @Test
+        fun countJavaSloc() {
+            assertThat(sourceCode.countJavaSloc()).isEqualTo(3)
+        }
+    }
+
+    @Nested
     inner class OptionalTest {
+
         @Test
         fun `toNullable() of empty Optional should result in null`() {
             val emptyOptional: Optional<String> = Optional.empty()
@@ -29,7 +82,20 @@ internal class ExtensionsTest {
     }
 
     @Nested
+    inner class FileTest {
+
+        @Test
+        fun plus() {
+            val testDirectory = File(this::class.java.getResource("/java/").path)
+            val testFile = File(this::class.java.getResource("/java/Example.java").path)
+
+            assertThat(testDirectory + testFile).isEqualTo(testFile)
+        }
+    }
+
+    @Nested
     inner class PairTest {
+
         @Test
         fun map() {
             assertThat(Pair(10, 20).map { it * it }).isEqualTo(Pair(100, 400))
@@ -37,17 +103,8 @@ internal class ExtensionsTest {
     }
 
     @Nested
-    inner class CollectionTest {
-        @Test
-        fun cartesianProduct() {
-            val value: List<Pair<Int, Int>> = listOf(1, 2, 3).cartesianProduct()
-            assertThat(value).contains(Pair(1, 2), Pair(1, 3), Pair(2, 3))
-            assertThat(value.flatMap { it.toList() }).containsExactlyInAnyOrder(1, 2, 3)
-        }
-    }
-
-    @Nested
     inner class NodeTest {
+
         private val exampleCompilationUnit = StaticJavaParser.parse(File(this::class.java.getResource("/java/Example.java").path))
 
         @Test
