cf18b36179125b508ed061520b494e0bf709c89a
Jakob Löhnertz <mail@jakob.codes>
2019-12-15 19:05:59 +0100

    Format code

--- backend/src/test/resources/hashTests/TestIdentifiers.java
+++ backend/src/test/resources/hashTests/TestIdentifiers.java
@@ -2,14 +2,14 @@ package testing;
 
 class TestIdentifiers {
     /**
-    * This is a comment
-    */
+     * This is a comment
+     */
     public static String hello() {
         String result = "hello";
         return result; // This is another comment
     }
 
     /*
-    * This is a block comment
-    */
+     * This is a block comment
+     */
 }
